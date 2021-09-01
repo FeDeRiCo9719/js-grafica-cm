20276cbde5a8f60b24caa6d090e7995311605665
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-01 16:22:17 +0200

    graphics

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,9 @@
 /* Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
 Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso. */
+
+// 1. Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco
+
+// 2. Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga
+
+// 3. Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso
